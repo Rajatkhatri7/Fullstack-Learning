e43c8d453c344cc7dc34cfd385c1b34c9858a54c
Rajat <rajatkhatri0002@gmail.com>
2020-11-08 00:39:31 +0530

    personalised greeting on visiting

--- Basic Web page/scripts/main.js
+++ Basic Web page/scripts/main.js
@@ -11,11 +11,46 @@
 
 let myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/me1.png') {
-    myImage.setAttribute ('src','images/me2.jpg');
-  } else {
-    myImage.setAttribute ('src','images/me1.png');
-  }
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/me1.png') {
+        myImage.setAttribute('src', 'images/me2.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/me1.png');
+    }
 }
+
+// Personalize greeting
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUsername() {
+
+    let myName = prompt('Enter your name : ');
+    if (!myName) {
+        setUsername();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Hey there, ' + myName;
+    }
+
+}
+
+
+// calling the functiion
+
+if (!localStorage.getItem('name')) {
+
+    setUsername();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hey there, ' + storedName;
+
+}
+
+myButton.onclick = function () {
+    setUsername();
+}
+
+// what if user does not enter a name in that case we use if else statement to chk condition  
