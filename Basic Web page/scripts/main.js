76b7ac45eaf03c26fddeeecb7bdb17302330abc1
Rajat <rajatkhatri0002@gmail.com>
2020-11-07 22:51:32 +0530

    javascript event handler function onclick() implemented

---
+++ Basic Web page/scripts/main.js
@@ -0,0 +1,6 @@
+// const myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello World!!';
+
+document.querySelector('img').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
