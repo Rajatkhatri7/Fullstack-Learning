e8c01578a15662b6bfe18e8018291b8e6ba9418f
Rajat <rajatkhatri0002@gmail.com>
2020-11-08 00:23:23 +0530

    onclick image change with javascrpt

--- Basic Web page/scripts/main.js
+++ Basic Web page/scripts/main.js
@@ -1,6 +1,21 @@
 // const myHeading = document.querySelector('h1');
 // myHeading.textContent = 'Hello World!!';
 
-document.querySelector('img').onclick = function() {
-    alert('Ouch! Stop poking me!');
+// document.querySelector('img').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+
+
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/me1.png') {
+    myImage.setAttribute ('src','images/me2.jpg');
+  } else {
+    myImage.setAttribute ('src','images/me1.png');
+  }
 }
